d05d6966890bd95f810aa714ddef2814f420d911
kyle-quach <109721786+kyle-quach@users.noreply.github.com>
2023-03-10 13:07:22 -0700

    Add city

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -50,10 +50,10 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void addCity(City city){
-        cities.add(city);
+//        cities.add(city);
     }
 
     public boolean hasCity(City city) {
-        return cities.contains(city);
+//        return cities.contains(city);
     }
 }
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> cityList = new ArrayList<>();
     @Test
     public void testAddCity() {
         CustomList list = new CustomList(null, cityList);
@@ -21,13 +22,13 @@ public class CustomListTest {
         assertEquals(1,list.getCount());
 
     }
-    @Test
-    public void testHasCity() {
-        CustomList list = new CustomList(null, cityList);
-        City city1 = new City("Regina", "Saskatchewan");
-        City city2 = new City("Edmonton", "AB");
-        list.addCity(city1);
-        assertTrue(list.hasCity(city1));
-        assertFalse(list.hasCity(city2));
-    }
+//    @Test
+//    public void testHasCity() {
+//        CustomList list = new CustomList(null, cityList);
+//        City city1 = new City("Regina", "Saskatchewan");
+//        City city2 = new City("Edmonton", "AB");
+//        list.addCity(city1);
+//        assertTrue(list.hasCity(city1));
+//        assertFalse(list.hasCity(city2));
+//    }
 }
